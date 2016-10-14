7653279566988f24105de589ccfd03ee5d7eb303
ryank1 <ryankangas@gmail.com>
2016-10-13 20:32:49 -0400

    Added financial class, added some getters/setters in Items/Player classes and added base price in Item
    Financial can store 2 different types of data. End of day data for
    statistics is stored in a list and then transaction data stored in a
    data table.
    
    Data table can be searched through by day, reason or value.
    Values can also be added up after searching for a reason.
    Need to change how date is stored probably.
    Also need know when the day ends to store end of day data.

---
+++ Assets/Player.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace tycoon
+{
+    public class Player : MonoBehaviour
+    {
+
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        double money;
+        int capacity = 100; // default
+        List<Item> inventory = new List<Item>();
+
+        void addItem(Item item)
+        {
+            if (inventory.Count >= capacity)
+                return;
+            inventory.Add(item);
+        }
+
+        void deleteItem(Item item)
+        {
+            if (!inventory.Contains(item))
+                return;
+            inventory.Remove(item);
+
+        }
+
+        void addItems(Item[] items)
+        {
+            if ((inventory.Count + items.Length) < capacity)
+                return;
+            for (int i = 0; i < items.Length; i++)
+            {
+                inventory.Add(items[i]);
+            }
+        }
+        public List<Item> getInventory()
+        {
+            return inventory;
+        }
+
+        public double getMoney()
+        {
+            return money;
+        }
+
+    }
+}
