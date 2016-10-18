20bfa9852969c3df1db90460c97225a2967d263d
RFonzi <ryan.fonzi@gmail.com>
2016-10-18 10:43:32 -0400

    Add PlayerData Singleton for storing savable data

---
+++ Assets/PlayerData.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerData {
+    private static PlayerData instance = new PlayerData();
+
+    private PlayerData() { }
+
+    public static PlayerData getInstance() {
+        return instance;
+    }
+    
+}
