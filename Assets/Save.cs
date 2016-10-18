98289a93f5de9a430be788c896774b1f47a17da7
RFonzi <ryan.fonzi@gmail.com>
2016-10-17 22:12:50 -0400

    Preliminary save support (Needs work)

---
+++ Assets/Save.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.IO;
+
+public class Save : MonoBehaviour {
+
+	public void saveData() {
+        BinaryFormatter bf = new BinaryFormatter();
+        FileStream file = File.Open(Application.persistentDataPath + "/save.dat", FileMode.Open);
+
+        Object replaceme = new Object(); //REPLACE THIS WITH REAL DATA OBJECT
+
+        bf.Serialize(file, replaceme); // Data obj goes into second param
+        file.Close();
+    }
+
+    public void loadData() {
+        if(File.Exists(Application.persistentDataPath + "/save.dat")) {
+            BinaryFormatter bf = new BinaryFormatter();
+            FileStream file = File.Open(Application.persistentDataPath + "/save.dat", FileMode.Open);
+
+            Object replaceme = (Object)bf.Deserialize(file); //REPLACE THIS WITH REAL DATA OBJECT
+            file.Close();
+        }
+    }
+}
