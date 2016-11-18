243a3e7e0cd3b780e5455af344655f3884281614
melissawetzel <mjw349@gmail.com>
2016-11-18 17:44:57 -0500

    Replaced Options panel. Progress on Lists.

---
+++ Assets/optionsScript.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class optionsScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        Image soundBlocker = GameObject.Find("soundBlockerImg").GetComponent<Image>();
+        soundBlocker.enabled = !soundBlocker.enabled;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    // toggle sound on click of soundToggleButton
+    public void soundToggle()
+    {
+        // switch On/Off state of button text and icon
+        Image soundBlocker = GameObject.Find("soundBlockerImg").GetComponent<Image>();
+        if (GameObject.Find("soundToggleButton").GetComponentInChildren<Text>().text == "Sound is On")
+        {
+            soundBlocker.enabled = !soundBlocker.enabled;
+            GameObject.Find("soundToggleButton").GetComponentInChildren<Text>().text = "Sound is Off";
+        }
+        else
+        {
+            GameObject.Find("soundToggleButton").GetComponentInChildren<Text>().text = "Sound is On";
+            soundBlocker.enabled = !soundBlocker.enabled;
+        }
+    }
+}
