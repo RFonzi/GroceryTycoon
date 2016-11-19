f25004d9fa0321cd4c61ff62113bf4a9fe16d1d7
melissawetzel <mjw349@gmail.com>
2016-11-19 17:31:06 -0500

    Added Lists, Item Panel.
    Item Panel's header bets updated based on the Inventory item button
    clicked.
    An untested method is in the Item class file.

---
+++ Assets/Item.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Events;
+using System.Collections;
+
+public class Item : MonoBehaviour {
+
+    public GameObject itemHeader;
+    public GameObject buttonText;
+    public GameObject prefabItemText;
+    public GameObject prefabButton;
+    public RectTransform ParentPanel;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    // display proper item in ItemPanel
+    public void displayItemMenu()
+    {
+        string itemName = buttonText.GetComponent<Text>().text;
+
+        for(int i=0; i<5; i++)
+        {
+            // for the item text
+            GameObject listItemText = (GameObject)Instantiate(prefabItemText);
+            listItemText.transform.SetParent(ParentPanel, false);
+            listItemText.transform.localScale = new Vector3(1, 1, 1);
+
+            //Text tempText = listItemText.GetComponent<Text>();
+            int tempInt = i;
+
+            //tempText.onClick.AddListener(() => ButtonClicked(tempInt));
+
+            // for "Remove" buttons
+            GameObject goButton = (GameObject)Instantiate(prefabButton);
+            goButton.transform.SetParent(ParentPanel, false);
+            goButton.transform.localScale = new Vector3(1, 1, 1);
+
+            Button tempButton = goButton.GetComponent<Button>();
+
+            tempButton.onClick.AddListener(() => ButtonClicked(tempInt));
+
+        }
+    }
+
+    // click item in inventory list
+    public void readType()
+    {
+        if(buttonText.GetComponent<Text>().text == "Orange Juice")
+        {
+            itemHeader.GetComponent<Text>().text = "OJ";
+        }
+        else
+        {
+            itemHeader.GetComponent<Text>().text = buttonText.GetComponent<Text>().text;
+        }
+        
+    }
+
+    void ButtonClicked(int buttonNo)
+    {
+        Debug.Log("Button clicked = " + buttonNo);
+    }
+}
---
+++ Assets/Utils.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Utils : MonoBehaviour {
+
+    // global program variables
+    public static string itemType;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
