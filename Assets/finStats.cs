3a304220a2b7d6de061824c70811cc47f439c2eb
ryank <ryankangas@gmail.com>
2016-12-01 23:05:37 -0500

    networth works in management

---
+++ Assets/finStats.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+namespace tycoon
+{
+    public class finStats : MonoBehaviour
+    {
+
+        // Use this for initialization
+        public Text text;
+
+        // Update is called once per frame
+        void Update()
+        {
+            SimState.Instance.sim.fin.calculateNetWorth(SimState.Instance.player.getMoney(), SimState.Instance.player.getInventory());
+            setText(SimState.Instance.sim.fin.getNetWorth().ToString());
+            //setText("100");
+        }
+        public void setText(string newtext)
+        {
+            var myText = gameObject.GetComponent<Text>();
+            myText.text = newtext;
+        }
+    }
+}
